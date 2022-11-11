242e6832c71952c00bb76c71df2bddf0c1617674
WoIfey <wuflerz@outlook.com>
2022-11-11 11:21:03 +0200

    Updates

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function scrollFunction() {
   var nav = document.getElementById("navbar");
 
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 750) {
-    nav.style.top = "-60px";
+    nav.style.top = "-80px";
     nav.style.transition = "0.2s";
-    nav.style.opacity = "50%";
+    nav.style.opacity = "30%";
   } else {
     nav.style.top = "0px";
     nav.style.transition = "0.2s";
